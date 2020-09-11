447f4dc04291a3b1810d81512e6abb104fe954f0
Nandhakishor <61514287+R-Nandhakishor@users.noreply.github.com>
2020-09-11 17:28:31 +0530

    Add files via upload

---
+++ Assignments/Nandhakishor/Assignment 1.py
@@ -0,0 +1,64 @@
+1. Output: 6,18
+
+2.#Program to input a string and print its length.
+
+Name=input("Enter the Name")
+print(len(Name)) 
+
+3. # Program to input 2 numbers and print their sum and difference.
+Num1=int(input('Enter the first number'))
+Num2=int(input('Enter the second number'))
+Diff=Num1-Num2
+print('The Difference is: ',Diff)
+
+4.abde
+5.abde
+
+6. abababab
+7.	ab 
+	ab
+	ab
+	ab
+8.# Program to input a string s and a number n. Print the string n times on the screen, each should appear in a separate line
+s=input('Enter the string')
+n=int(input('Enter the number'))
+print(s*n, end=" ")
+
+9. Gaurav
+   None
+
+10. int
+
+11. str,26
+
+12. sqrt
+
+13. #Program to input a number and print its square root ().
+import math
+Num=int(input('Enter the number'))
+SQR=math.sqrt(Num)
+print(SQR)
+
+14. #Program to input 4 numbers from user and print their average
+
+a=int(input('Enter the 1st number'))
+b=int(input('Enter the 2nd number'))
+c=int(input('Enter the 3rd number'))
+d=int(input('Enter the 4th number'))
+
+print('THe Average is: ',(a+b+c+d)/4)
+
+15. abs-> provides the absolute value of an argument.
+    syntax: abs(value)-> takes one argument
+
+16. __main__
+
+17.__main__
+
+18. No
+
+19. __main__
+    bulitins
+    int
+
+
