d4445565fb577ba9757f58db8c83e40f10c7dd9c
bhardwaj147 <prashantbhardwaj147@gmail.com>
2020-10-03 02:12:37 +0530

    AdvanceFunctions_File_Numpy_Assigments_committed

---
+++ Assignments/Prashant/AssignmentPython9Files/readprogram.py
@@ -0,0 +1,5 @@
+# Program to read content of the file 
+import sys
+
+with open (sys.argv[0],"r") as f:
+    print (f.read())
---
+++ Assignments/Prashant/AssignmentPython9Files/readspace.py
@@ -0,0 +1,3 @@
+with open ("original_file", "r") as f:
+    text = f.read ()
+    print ("Total no of spaces in file = ", text.count (" "))
